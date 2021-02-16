0feb04041e5cb9885a5fd70549632a96d48f72db
freejob985 <freejob985@gmail.com>
2021-02-16 14:09:52 +0200

    شسي

--- resources/views/home/inc/latest.blade.php
+++ resources/views/home/inc/latest.blade.php
@@ -146,12 +146,8 @@ if (isset($latestOptions, $latestOptions['hide_on_mobile']) and $latestOptions['
 									@if (isset($liveCat->type))
 										@if (!in_array($liveCat->type, ['not-salable']))
 											@if ($post->price > 0)
-											@if (\App\Helpers\Number::money($post->price) == "--")
-											<small class="label badge-success">
-												{{ t('negotiable') }}</small>
-												@else
+											
 												{!! \App\Helpers\Number::money($post->price) !!}
-												@endif
 											@else
 											//	{!! \App\Helpers\Number::money('--') !!}
 											@endif
